42b45224dfbda3193334f9f73b31e3574feb0a81
R1N-K0 <peanut904018@gmail.com>
2025-08-07 14:39:42 +0900

    ポートの設定

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3001;
   app.enableCors({
-    origin: "http://localhost:3000",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
+    origin: "*",
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
   })
   await app.listen(process.env.PORT ?? 3000);
 }
